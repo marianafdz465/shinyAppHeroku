fc20d8417b3ba3734c70b408ab37931544ca6c77
marianafdz465 <marianafdz465@gmail.com>
2021-03-07 21:42:01 -0600

    First commit to deploy with Heroku

---
+++ run.R
@@ -0,0 +1,20 @@
+##************************************************************************
+## Script Name: run.R
+## Purpose:
+##
+##
+## Created:
+## Authors: Mariana Fernandez
+##
+## GitHub: marianafdz465
+##
+##
+##************************************************************************
+##*
+
+
+library(shiny)port <- Sys.getenv('PORT')shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
+)
